565fe0fbc6293d50cbad30fa70c713ca6ef29d8f
Himanshu Kumar <himanshu.k822887@gmail.com>
2024-06-01 18:24:50 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://feasible-marmoset-71.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
